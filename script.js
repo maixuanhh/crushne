01d1e5d8dbbcfa9e167d1ef07db3f4afcc67d9ba
maixuanhh <miniupi04@gmail.com>
2025-06-17 19:36:12 +0700

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const noBtn = document.querySelector(".no-btn");
 const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Anh đúng là đồ tòyyyyyyy 🥺<br>dỗiiii x10000000";
+  question.innerHTML = "Bé tệ với chị quáaaaaaa 🥺<br>dỗiiii x10000000";
   gif.src =
     "https://media.giphy.com/media/vvc1dJPLEU2QOw9cXy/giphy.gif";
 
